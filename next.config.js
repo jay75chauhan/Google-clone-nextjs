f737ebbb012626cd5f6783e434cfbe14b80571e1
jay75chauhan <chauhan75jayk@gmail.com>
2021-10-10 15:52:14 +0530

    pwa add

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
-module.exports = {
-   eslint: {
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    // disable: process.env.NODE_ENV === "development",
+  },
+  eslint: {
     // Warning: Dangerously allow production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
@@ -7,4 +15,4 @@ module.exports = {
   images: {
     domains: ["www.google.co.in"],
   },
-};
+});
